a15d97de463b76ea4f35c916437f5d7b4c17f3ab
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-03-04 17:32:30 +0530

    add three column layout for search

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -44,21 +44,34 @@ class Search {
     }
 
     getResults() {
-        $.when(
-            $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), 
-            $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-        ).then((posts, pages) => {
-            var combinedResults = posts[0].concat(pages[0]);
-                this.resultDiv.html(`
+        $.getJSON(universityData.root_url + '/wp-json/university/v1/search?term=' + this.searchField.val(), (results) => {
+            this.resultDiv.html(`
+                <div class="row">
+                <div class="one-third">
                 <h2 class="search-overlay__section-title">General Information</h2>
-                ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
-                ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type == 'post' ? `by ${item.authorName}` : ''}</li>`).join('')}
-                ${combinedResults.length ? '</ul>' : ''}
+                ${results.generalInfo.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
+                ${results.generalInfo.map(item => `<li><a href="${item.permalink}">${item.title}</a> ${item.postType == 'post' ? `by ${item.authorName}` : ''}</li>`).join('')}
+                ${results.generalInfo.length ? '</ul>' : ''}
+                </div>
+                <div class="one-third">
+                <h2 class="search-overlay__section-title">Programs</h2>
+                ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No programs matches that search. <a href="${universityData.root_url}/programs">View All Programs</a></p>`}
+                ${results.programs.map(item => `<li><a href="${item.permalink}">${item.title}</a> </li>`).join('')}
+                ${results.programs.length ? '</ul>' : ''}
+                <h2 class="search-overlay__section-title">Professors</h2>
+                </div>
+                <div class="one-third">
+                <h2 class="search-overlay__section-title">Campuses</h2>
+                ${results.campuses.length ? '<ul class="link-list min-list">' : '<p>No campuses matches that search.</p>'}
+                ${results.campuses.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join('')}
+                ${results.campuses.length ? '</ul>' : ''}
+                <h2 class="search-overlay__section-title">Events</h2>
+                </div>
+                </div>
                 `);
                 this.isSpinnerVisible = false;
-        }, () => {
-            this.resultDiv.html('<p>Unexpected Error, Please try again.</p>')
         });
+
     }
 
     keyPressDispatcher(e) {
