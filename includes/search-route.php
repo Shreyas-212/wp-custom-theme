aa796dba5dfb624fee05c9718206ef474028405c
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-03-04 17:33:02 +0530

    add query

--- includes/search-route.php
+++ includes/search-route.php
@@ -36,7 +36,9 @@ function universitySearchResults($data) {
         if(get_post_type() == 'professor') {
             array_push($results['professors'], array(
             'title' => get_the_title(),
-            'permalink' => get_the_permalink()
+            'permalink' => get_the_permalink(),
+            'postType' => get_post_type(),
+            'authorName' => get_the_author()
         ));
         }
 
