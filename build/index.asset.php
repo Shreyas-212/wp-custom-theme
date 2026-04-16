946aefd0aa8d5aab41b733e1ea6d487f5580862e
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-04-16 15:04:46 +0530

    add per user post limit code

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '817a85b3e01a3b33fffc');
+<?php return array('dependencies' => array('jquery'), 'version' => '3e3dbb6c0a775872d5ae');
--- build/index.js
+++ build/index.js
@@ -2235,6 +2235,9 @@ class MyNotes {
         this.makeNoteReadOnly(thisNote);
         console.log("Congrats");
         console.log(response);
+        if (response.userNoteCount < 5) {
+          jquery__WEBPACK_IMPORTED_MODULE_0___default()(".note-limit-message").removeClass("active");
+        }
       },
       error: response => {
         console.log("Sorry");
@@ -2270,6 +2273,9 @@ class MyNotes {
         console.log(response);
       },
       error: response => {
+        if (response.responseText = "You have reached your note limit.") {
+          jquery__WEBPACK_IMPORTED_MODULE_0___default()(".note-limit-message").addClass("active");
+        }
         console.log("Sorry");
         console.log(response);
       }
