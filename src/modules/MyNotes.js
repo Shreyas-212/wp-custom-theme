a018145a7744a91efb3dd000224c03e7d014e76e
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-04-16 15:04:27 +0530

    add per user post limit code

--- src/modules/MyNotes.js
+++ src/modules/MyNotes.js
@@ -73,6 +73,9 @@ class MyNotes {
                 this.makeNoteReadOnly(thisNote);
                 console.log("Congrats");
                 console.log(response);
+                if(response.userNoteCount < 5) {
+                    $(".note-limit-message").removeClass("active");
+                }
             },
             error: (response) => {
                 console.log("Sorry");
@@ -110,6 +113,9 @@ class MyNotes {
                 console.log(response);
             },
             error: (response) => {
+                if(response.responseText = "You have reached your note limit.") {
+                    $(".note-limit-message").addClass("active");
+                }
                 console.log("Sorry");
                 console.log(response);
             }
