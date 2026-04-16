277625301bd11e7c48f0999d97fa910e737667d7
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-04-16 15:04:00 +0530

    add per user post limit code

--- functions.php
+++ functions.php
@@ -8,6 +8,12 @@ function university_custom_rest() {
             return get_the_author();
         }
     ));
+
+    register_rest_field('note', 'userNoteCount', array(
+        'get_callback' => function() {
+            return count_user_posts(get_current_user_id(), 'note');
+        }
+    ));
 }
 
 add_action('rest_api_init', 'university_custom_rest');
