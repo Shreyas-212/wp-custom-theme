3794d14c85cb7ccc23ac233b95bcb60b8328c0d9
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-04-18 17:26:21 +0530

    add create like post code

--- includes/like-route.php
+++ includes/like-route.php
@@ -3,19 +3,28 @@
 add_action('rest_api_init', 'universityLikeRoutes');
 
 function universityLikeRoutes() {
-    register_rest_route('university//v1', 'manageLike', array(
+    register_rest_route('university/v1', 'manageLike', array(
         'methods' => 'POST',
         'callback' => 'createLike'
     ));
 
-    register_rest_route('university//v1', 'manageLike', array(
+    register_rest_route('university/v1', 'manageLike', array(
         'methods' => 'DELETE',
         'callback' => 'deleteLike'
     ));
 }
 
-function createLike() {
-    return 'Thanks';
+function createLike($data) {
+    $professor = sanitize_text_field($data['professorId']);
+
+    wp_insert_post(array(
+        'post_type' => 'like',
+        'post_status' => 'publish',
+        'post_title' => 'Second PHP test',
+        'meta_input' => array(
+            'liked_professor_id' => $professor
+        )
+    ));
 }
 
 function deleteLike() {
